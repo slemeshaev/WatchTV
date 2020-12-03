dc4a4ba48ccbb54b1a6ae5a679da055f97c2ea99
slemeshaev <lemeshaevs@gmail.com>
2020-12-03 11:01:23 +0300

    files for project

---
+++ WatchTV/WatchTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchTV
+//
+//  Created by Станислав Лемешаев on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
